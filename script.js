260bea4432252c16b8a67cfe6fa42e21e35b7c1e
NicolasLeonardoo <nicolasleonardo001@gmail.com>
2025-08-09 22:26:02 -0300

    backup

--- script.js
+++ script.js
@@ -5,20 +5,36 @@ let verMaisButtons = document.querySelectorAll('.verMais');
 let carrossel = document.querySelector('.carrossel');
 let listaHTML = document.querySelector('.carrossel .lista');
 
-proxButton.onclick = function() {
+proxButton.onclick = function () {
     showSlider('proximo');
 }
-antButton.onclick = function() {
+antButton.onclick = function () {
     showSlider('anterior');
 }
+let unAccceptClick;
 const showSlider = (type) => {
+    proxButton.style.pointerEvents = 'none';
+    antButton.style.pointerEvents = 'none';
+
+    carrossel.classList.remove('proximo', 'anterior');
     let items = document.querySelectorAll('.carrossel .lista .item');
-    if(type == 'proximo'){
+    if (type == 'proximo') {
         listaHTML.appendChild(items[0]);
         carrossel.classList.add('proximo');
-    }else{
+    } else {
         let ultimo = items.length - 1;
         listaHTML.prepend(items[ultimo]);
         carrossel.classList.add('anterior');
     }
+    clearTimeout(unAccceptClick);
+    unAccceptClick = setTimeout(() => {
+        proxButton.style.pointerEvents = 'auto';
+        antButton.style.pointerEvents = 'auto';
+    }, 2000);
 }
+
+verMaisButtons.forEach(button => {
+    button.onclick = function() {
+        carrossel.classList.add('mostrarMais');
+    }
+})
